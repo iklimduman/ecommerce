64a001fab8eb93a839f8caa0f129080f40dc3dc4
iklimduman <iklimdumann@gmail.com>
2021-09-01 22:34:46 +0300

    add max space size to package.json and add material ui icons to navbar buttons

---
+++ src/Components/Common/Buttons/Buttons.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from '@material-ui/icons/Search';
+import StarIcon from '@material-ui/icons/Star';
+import HomeIcon from '@material-ui/icons/Home';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PersonIcon from '@material-ui/icons/Person';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        position: 'absolute',
+        color: 'rgb(24,0,67)',
+        background: 'white',
+        '&:hover': {
+            background: "rgb(233,216,250);",
+        },
+        padding: '4px !important',
+        height: 26,
+        width: 30,
+        borderRadius: '0 3px 3px 0',
+        marginLeft: '10px',
+        top: 0
+    },
+});
+
+const useStylesNavbar = makeStyles({
+    root: {
+        color: 'white',
+    }
+})
+
+const SearchButton = () => {
+    const classes = useStyles();
+    const className = clsx(classes.root);
+
+    return (
+        <IconButton>
+            <SearchIcon className={className} />
+        </IconButton>
+    )
+}
+
+const HomeButton = () => {
+    const classesNavbar = useStylesNavbar();
+    const classNameNavbar = clsx(classesNavbar.root);
+
+    return (
+        <IconButton>
+            <HomeIcon className={classNameNavbar} />
+        </IconButton>
+    )
+}
+
+const FavButton = () => {
+    const classesNavbar = useStylesNavbar();
+    const classNameNavbar = clsx(classesNavbar.root);
+
+    return (
+        <IconButton>
+            <StarIcon className={classNameNavbar} />
+        </IconButton>
+
+    )
+}
+
+const ShoppingCartButton = () => {
+    const classesNavbar = useStylesNavbar();
+    const classNameNavbar = clsx(classesNavbar.root);
+
+    return (
+        <IconButton>
+            <ShoppingCartIcon className={classNameNavbar} />
+        </IconButton>
+
+    )
+}
+
+const ProfileButton = () => {
+    const classesNavbar = useStylesNavbar();
+    const classNameNavbar = clsx(classesNavbar.root);
+
+    return (
+        <IconButton>
+            <PersonIcon className={classNameNavbar} />
+        </IconButton>
+
+    )
+}
+
+export {
+    SearchButton,
+    HomeButton,
+    FavButton,
+    ShoppingCartButton,
+    ProfileButton,
+}
--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -3,13 +3,7 @@ import React, { useState } from "react";
 import MenuItems from "../MenuItem/MenuItem";
 import "./Navbar.css";
 import SearchBar from "../Serach-Bar/SearchBar";
-
-function CreateMenuItem(data) {
-    return (
-        <a className={data.className} href={data.url}>
-            {data.title}
-        </a>)
-}
+import { HomeButton, FavButton, ShoppingCartButton, ProfileButton } from "../Buttons/Buttons";
 
 function Navbar() {
 
@@ -21,14 +15,22 @@ function Navbar() {
 
             <div class="topnav-centered">
                 <a href="#home" class="active">Logo</a>
-                
+
             </div>
             <div>
-                <SearchBar className="topnav-search"/>
+                <SearchBar className="topnav-search" />
             </div>
 
             <div class="topnav-right">
-                {MenuItems.map(CreateMenuItem)}
+                <HomeButton />
+                <a>Home</a>
+                <FavButton />
+                <a>My Favs</a>
+
+                <ShoppingCartButton />
+                <a>Shopping Cart</a>
+                <ProfileButton />
+                <a>My Account</a>
             </div>
 
         </div>
